2bd9e49059c605845613b86c155d7938c6577e05
Mariano Lischetti <mlischetti@despegar.com>
2016-04-06 11:55:14 -0300

    Create documentType model

---
+++ src/main/java/com/kineo/model/DocumentType.java
@@ -0,0 +1,17 @@
+package com.kineo.model;
+
+/**
+ * Created by mlischetti on 4/6/16.
+ */
+public enum DocumentType {
+    DNI("Documento nacional de identidad"),
+    LE("Libreta de enrolamiento"),
+    LC("Libreta civica"),
+    CI("Cedula de identidad");
+
+    private String description;
+
+    DocumentType(String description) {
+        this.description = description;
+    }
+}
